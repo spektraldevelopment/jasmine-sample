632529d2b69e5a37983bbc1a7e3fd3e1acabee44
SpektralDevelopment <spektraldevelopment@gmail.com>
2014-02-15 19:36:01 -0500

    Familiarized with matchers in Jasmine.

--- jasmine-standalone-2.0.0/spec/FooLibSpec.js
+++ jasmine-standalone-2.0.0/spec/FooLibSpec.js
@@ -30,8 +30,37 @@ describe("FooLib", function() {
         expect(fooLib.bar).toBeUndefined();
     });
 
-    it("testToBeNull should return null.", function() {
+    it("testToBeNull() should return null.", function() {
         expect(fooLib.testToBeNull()).toBeNull();
+    });
+
+    it("testToBeTruthy() should return true.", function() {
+        expect(fooLib.testToBeTruthy()).toBeTruthy();
+    });
+
+    it("testToBeFalsy() should return false.", function() {
+        expect(fooLib.testToBeFalsy()).toBeFalsy();
+    });
+
+    it("testToContain() returns array with value: bar.", function () {
+        expect(fooLib.testToContain()).toContain("bar");
+    });
+
+    it("testToBeLessThan() is less than 5.", function () {
+        expect(fooLib.testToBeLessThan()).toBeLessThan(5);
+    });
+
+    it("testToBeGreaterThan() is greater than 2.", function () {
+        expect(fooLib.testToBeGreaterThan()).toBeGreaterThan(2);
+    });
+
+    it("testToBeCloseTo() is close to 2.54. ", function () {
+        //toBeCloseTo allows you to check if a number is close to another number, given a certain amount of decimal precision as the second argument.
+        expect(fooLib.testToBeCloseTo()).toBeCloseTo(2.54, 2);
+    });
+
+    it("testToThrow() threw an error.", function () {
+        expect(fooLib.testToThrow).toThrow();
     });
 
 //    Included matchers:
--- jasmine-standalone-2.0.0/src/FooLib.js
+++ jasmine-standalone-2.0.0/src/FooLib.js
@@ -18,4 +18,39 @@ function FooLib (ID) {
     this.testToBeNull = function () {
         return null;
     }
+
+    //To demonstrate toBeTruthy
+    this.testToBeTruthy = function () {
+        return true;
+    }
+
+    //To demonstrate toBeFalsy
+    this.testToBeFalsy = function () {
+        return false;
+    }
+
+    //To demonstrate toContain
+    this.testToContain = function () {
+        return ["foo", "bar", "lib"];
+    }
+
+    //To demonstrate toBeLessThan
+    this.testToBeLessThan = function () {
+        return 2;
+    }
+
+    //To demonstrate toBeLessThan
+    this.testToBeGreaterThan = function () {
+        return 5;
+    }
+
+    //To demonstrate toBeCloseTo
+    this.testToBeCloseTo = function () {
+        return 2.5378;
+    }
+
+    //To demonstrate toThrow
+    this.testToThrow = function () {
+        return a + 1;
+    }
 };
